5ac0f9a09dc42ed3da79b47cac3ce73e31256397
Jonas Almeida <jalmeida@mathbiol.org>
2016-12-06 10:57:54 -0500

    parsing sample VCF

--- vcfProto.js
+++ vcfProto.js
@@ -3,13 +3,21 @@ console.log('vcfProto.js loaded');
 // prototypes for Michael Conchez
 
 vcfProto=function(vcf){
-    var bd = vcf.data.body
+    var bd = vcf
     
     4
 }
 
-// run it if this is being called from Tomasz VCF tool
-if(VCF){
-    vcfProto(VCF.dir.vcfs[2])
+vcfProto.UI=function(div){
+    var div = document.getElementById('vcfProtoDiv')
+    console.log('UI not dev yet')
+    // get instance of a VCF and parse it to VCF
+    $.get('someMRSA.vcf')
+     .then(function(x){
+         var vcf = (VCF.parse(x)).body
+         4
+     })
 }
+
+
 
