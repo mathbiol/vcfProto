271bb7b714c72898610a554d3b95afcbe70768c9
Jonas Almeida <jalmeida@mathbiol.org>
2016-12-06 13:56:01 -0500

    simple root parm addition

--- vcfProto.js
+++ vcfProto.js
@@ -27,11 +27,42 @@ vcfProto=function(url,fun){
 
 vcfProto.UI=function(div){
     var div = document.getElementById('vcfProtoDiv')
-    console.log('UI not dev yet')
     var someVCFurl = 'https://mathbiol.github.io/vcfProto/someMRSA.vcf'
-    var vp = vcfProto(someVCFurl,function(vp){
-        console.log('prototype extracted from '+someVCFurl,vp)        
-    })
+    
+    // assemble UI
+    var h ='<h3>Converting a VCF file into a JSON favored prototype call</h3>'
+    h += '<hr>'
+    h += 'Source file: <input id="vcfUrlInput" size="50" style="color:blue">'
+    h += '<hr>'
+    h += '<div id="vcfTxtDiv"><b>VCF text</b> [<span id="vcfTxtShowHide" style="color:blue">hide</span>]:<pre id="vcfTxtPre">...</pre></div>'
+    h += '<hr>'
+    h += '<div id="vcfJsonDiv"><b>VCF JSON</b> [<span id="vcfJsonShowHide" style="color:blue">hide</span>]:<pre id="vcfJsonPre">...</pre></div>'
+    h += '<hr>'
+    div.innerHTML=h
+    vcfUrlInput.value=someVCFurl
+    vcfTxtPre.style.fontSize="8"
+    vcfTxtPre.style.color="green"
+    $.get(someVCFurl)
+     .then(function(x){
+         vcfTxtPre.textContent=x
+         var vp = vcfProto(someVCFurl,function(vp){
+            console.log('prototype extracted from '+someVCFurl,vp)
+            vcfJsonPre.textContent=JSON.stringify(vp,null,3)
+         })
+     })
+    vcfTxtShowHide.onclick=vcfJsonShowHide.onclick=function(){
+        var pre = $('pre',this.parentElement)[0]
+        if(this.textContent=='hide'){
+            this.textContent='show'
+            pre.hidden=true
+        }else{
+            this.textContent='hide'
+            pre.hidden=false
+        }
+        4
+    }
+    4
+
     
 }
 
