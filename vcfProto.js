855eb1a518d36351b7ebfee2fae55ae7fdd054fe
Jonas Almeida <jalmeida@mathbiol.org>
2016-12-06 12:43:59 -0500

    parsing vcf txt to JSON prototype syntax

--- vcfProto.js
+++ vcfProto.js
@@ -2,21 +2,37 @@ console.log('vcfProto.js loaded');
 
 // prototypes for Michael Conchez
 
-vcfProto=function(vcf){
-    var bd = vcf
-    
-    4
+vcfProto=function(url,fun){
+    fun = fun || function(vp){ // default callback function to handle vcf prototype
+        console.log('prototype extracted from '+url,vp)
+    }
+    var src={}
+    src.base=url.match(/.+\//)[0] || ""
+    src.id=url.slice(src.base.length)
+    $.get(src.id)
+     .then(function(x){
+         var vcf = VCF.parse(x)
+         var vcfProto = {
+             id:src.id,
+             base:src.base
+         }
+         // add
+         vcfProto.add={
+             head:vcf.head,
+             body:vcf.body
+         }
+         fun(vcfProto)
+     })
 }
 
 vcfProto.UI=function(div){
     var div = document.getElementById('vcfProtoDiv')
     console.log('UI not dev yet')
-    // get instance of a VCF and parse it to VCF
-    $.get('someMRSA.vcf')
-     .then(function(x){
-         var vcf = (VCF.parse(x)).body
-         4
-     })
+    var someVCFurl = 'https://mathbiol.github.io/vcfProto/someMRSA.vcf'
+    var vp = vcfProto(someVCFurl,function(vp){
+        console.log('prototype extracted from '+someVCFurl,vp)        
+    })
+    
 }
 
 
