7edeb9d95b59d678546b9b645b685948bcd8eb94
Jonas Almeida <jalmeida@mathbiol.org>
2016-12-06 10:20:15 -0500

    sample VCF file added

--- vcfProto.js
+++ vcfProto.js
@@ -1,1 +1,15 @@
 console.log('vcfProto.js loaded');
+
+// prototypes for Michael Conchez
+
+vcfProto=function(vcf){
+    var bd = vcf.data.body
+    
+    4
+}
+
+// run it if this is being called from Tomasz VCF tool
+if(VCF){
+    vcfProto(VCF.dir.vcfs[2])
+}
+
